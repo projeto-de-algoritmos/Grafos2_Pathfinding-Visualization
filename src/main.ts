2a25ba0ebf4239b716c7457c697bec6fb5ed04c5
Guilherme Puida <guilhermepmoreira00@gmail.com>
2022-12-04 03:49:25 -0300

    Adding the rest of the input handlers

--- src/main.ts
+++ src/main.ts
@@ -14,14 +14,60 @@ const export$ = document.getElementById("export") as HTMLButtonElement;
 const walls$ = document.getElementById("walls") as HTMLTextAreaElement;
 
 const board = new Board(800, 800, canvas$, 20);
+
 let speed = 300;
 let timeout: number | undefined = undefined;
+
 const makeAnimationTimeout = (fps: number): number =>
   setTimeout(() => window.requestAnimationFrame(draw), 1000 / fps);
 
+fps$.addEventListener("change", (e) => {
+  speed = Number((e.target as HTMLInputElement).value);
+  clearTimeout(timeout);
+  timeout = makeAnimationTimeout(speed);
+});
+
+reset$.addEventListener("click", () => {
+  board.unlock();
+  board.reset(false);
+  board.render();
+});
+
+resetWalls$?.addEventListener("click", () => {
+  board.unlock();
+  board.reset(true);
+  board.render();
+})
+
+start$.addEventListener("click", () => {
+  if (board.locked) return;
+
+  board.lock();
+
+  switch (strategy$.value) {
+    case "bfs":
+      board.startBfs();
+      break;
+    case "dfs":
+      board.startDfs();
+      break;
+    case "a*-euclideana":
+      board.startAStar(euclideanDistance);
+      break;
+    case "a*-manhattan":
+      board.startAStar(manhattanDistance);
+  }
+});
+
+import$.addEventListener("click", () => board.deserialize(walls$.value));
+export$.addEventListener("click", () => {
+  walls$.value = board.serialize();
+});
 
 const draw = () => {
   board.render();
+
   timeout = makeAnimationTimeout(speed);
 };
+
 draw();
