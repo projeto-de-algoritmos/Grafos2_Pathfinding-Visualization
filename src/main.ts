57e9d23b3a9aa9c7a892d63011c666ef355a7d52
Guilherme Puida <guilhermepmoreira00@gmail.com>
2022-12-04 03:49:13 -0300

    Basic drawing logic

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import "./style.css";
+import Board from "./board";
+import { euclideanDistance, manhattanDistance } from "./position";
 
 const canvas$ = document.getElementById("board") as HTMLCanvasElement;
 const fps$ = document.getElementById("speed") as HTMLInputElement;
@@ -11,5 +13,15 @@ const import$ = document.getElementById("import") as HTMLButtonElement;
 const export$ = document.getElementById("export") as HTMLButtonElement;
 const walls$ = document.getElementById("walls") as HTMLTextAreaElement;
 
+const board = new Board(800, 800, canvas$, 20);
 let speed = 300;
 let timeout: number | undefined = undefined;
+const makeAnimationTimeout = (fps: number): number =>
+  setTimeout(() => window.requestAnimationFrame(draw), 1000 / fps);
+
+
+const draw = () => {
+  board.render();
+  timeout = makeAnimationTimeout(speed);
+};
+draw();
